d45b3016686072f69b05bf67beaf5e9e4aca3d64
NarminGale <narmingale@gmail.com>
2021-09-21 14:29:41 +0400

    Displayed data

--- 02-Tours/src/Tour.js
+++ 02-Tours/src/Tour.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
-function Tour() {
-  return <h2>tours component</h2>
+function Tour({ id, name, image, info, price }) {
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>{info}</p>
+        <button className="delete-btn">not interested</button>
+      </footer>
+    </article>
+  )
 }
 
 export default Tour
