be170feeead233219604374bd495296e64988513
NarminGale <narmingale@gmail.com>
2021-09-21 14:52:31 +0400

    Completed whole project

--- 02-Tours/src/Tours.js
+++ 02-Tours/src/Tours.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Tour from './Tour'
-function Tours({ tours }) {
+function Tours({ tours, removeTour }) {
   return (
     <section>
       <div className="title">
@@ -9,7 +9,7 @@ function Tours({ tours }) {
       </div>
       <div>
         {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour} />
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />
         })}
       </div>
     </section>
