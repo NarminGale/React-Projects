2c4b17f1eaf6f498c6e99993fb045334a1870dec
NarminGale <narmingale@gmail.com>
2021-09-21 14:39:15 +0400

    Completed functionality of toggle button

--- 02-Tours/src/Tour.js
+++ 02-Tours/src/Tour.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Tour({ id, name, image, info, price }) {
+  const [readMore, setReadMore] = useState(false)
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -9,7 +11,12 @@ function Tour({ id, name, image, info, price }) {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'show less' : 'read more'}
+          </button>
+        </p>
         <button className="delete-btn">not interested</button>
       </footer>
     </article>
