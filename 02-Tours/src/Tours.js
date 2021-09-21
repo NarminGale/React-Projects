da9f3d6781680e062ddf69b99dc7a16782dc559c
NarminGale <narmingale@gmail.com>
2021-09-21 14:29:48 +0400

    Displayed data

--- 02-Tours/src/Tours.js
+++ 02-Tours/src/Tours.js
@@ -1,7 +1,19 @@
 import React from 'react'
-
-function Tours() {
-  return <h2>tours component</h2>
+import Tour from './Tour'
+function Tours({ tours }) {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} />
+        })}
+      </div>
+    </section>
+  )
 }
 
 export default Tours
