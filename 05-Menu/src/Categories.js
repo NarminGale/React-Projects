6d6ddeccc1011d2b95b2e32873320c7b3ad6f2d0
NarminGale <narmingale@gmail.com>
2021-09-22 20:48:58 +0400

    Created buttons container

--- 05-Menu/src/Categories.js
+++ 05-Menu/src/Categories.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
-function Categories() {
-  return <div></div>
+function Categories({ filterItems }) {
+  return (
+    <div className="btn-container">
+      <button
+        type="button"
+        className="btn filter-btn"
+        onClick={() => {
+          filterItems('all')
+        }}>
+        all
+      </button>
+    </div>
+  )
 }
 
 export default Categories
