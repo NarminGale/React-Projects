aa4dad0f2c78964ffdab248a19fb88a784c38691
NarminGale <narmingale@gmail.com>
2021-09-22 21:11:48 +0400

    Setup Tabs project files

---
+++ 06-Tabs/src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
+
+const url = 'https://course-api.com/react-tabs-project'
+function App() {
+  return <></>
+}
+
+export default App
