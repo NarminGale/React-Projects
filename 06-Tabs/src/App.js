587749742112bd15129e3ac60afb34e2af9f8952
NarminGale <narmingale@gmail.com>
2021-09-23 09:12:02 +0400

    Fetch jobs form url

--- 06-Tabs/src/App.js
+++ 06-Tabs/src/App.js
@@ -3,6 +3,29 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    const response = await fetch(url)
+    const newJobs = awaitresponse.json()
+    setJobs(newJobs)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>loading...</h1>
+      </section>
+    )
+  }
+
   return <></>
 }
 
