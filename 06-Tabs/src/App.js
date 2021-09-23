82524eee32bd05dafc142f03618c984b391832d2
NarminGale <narmingale@gmail.com>
2021-09-23 10:03:41 +0400

    Completed project

--- 06-Tabs/src/App.js
+++ 06-Tabs/src/App.js
@@ -35,6 +35,21 @@ function App() {
         <div className="underline"></div>
       </div>
       <div className="jobs-center">
+        {/* button container */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                className={`job-btn ${index === value && 'active-btn'}`}
+                onClick={() => setValue(index)}>
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+
+        {/* job info */}
         <article className="job-info">
           <h3>{title}</h3>
           <h4>{company}</h4>
