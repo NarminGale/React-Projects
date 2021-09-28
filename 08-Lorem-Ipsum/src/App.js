0c50fb9dd3e6931efbf4bcbd6c4b65f9bd6959a4
NarminGale <narmingale@gmail.com>
2021-09-28 23:39:19 +0400

    Setup lorem-ipsum project files

--- 08-Lorem-Ipsum/src/App.js
+++ 08-Lorem-Ipsum/src/App.js
@@ -1,7 +1,46 @@
 import React, { useState } from 'react'
 import data from './data'
 function App() {
-  return <></>
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <section className="section-center">
+      <h3>tired of boring ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="value"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centurie.
+        </p>
+        <p>
+          But also the leap into electronic typesetting, remaining essentially
+          unchanged. It was popularised in the 1960s with the release of
+          Letraset sheets containing Lorem Ipsum passages, and more recently
+          with desktop publishing software like Aldus PageMaker including
+          versions of Lorem Ipsum.
+        </p>
+      </article>
+    </section>
+  )
 }
 
 export default App
