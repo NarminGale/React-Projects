7f2f7a876cd83be9c8e0285357a45dbbc97033ea
NarminGale <narmingale@gmail.com>
2021-10-08 23:12:51 +0400

    Added items to list

--- 10-Grocery-Bud/src/App.js
+++ 10-Grocery-Bud/src/App.js
@@ -11,6 +11,17 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    console.log('you pressed')
+    if (!name) {
+      // display alert
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name }
+      setList([...list, newItem])
+      setName('')
+    }
   }
   return (
     <section className="section-center">
@@ -23,17 +34,21 @@ function App() {
             className="grocery"
             placeholder="e.g. eggs"
             value={name}
-            onchange={(e) => setName(e.target.value)}
+            onChange={(e) => {
+              setName(e.target.value)
+            }}
           />
           <button type="button" className="submit-btn">
             {isEditing ? 'edit' : 'submit'}
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List />
-        <button className="clear-btn">clear items</button>
-      </div>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
+      )}
     </section>
   )
 }
