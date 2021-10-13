6c5db1861cff1772abc3f96a0bb2c5cf3015030a
NarminGale <narmingale@gmail.com>
2021-10-13 10:29:42 +0400

    Imported react icons

--- 11-Navbar/src/Navbar.js
+++ 11-Navbar/src/Navbar.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
+import { FaBars, FaTwitter } from 'react-icons/fa'
+import { links, social } from './data'
+import logo from './me-logo.png'
 
 function Navbar() {
   return <div></div>
