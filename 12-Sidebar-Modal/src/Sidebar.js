6d31a893531d5334593649b6b25b3630a8ee6e25
NarminGale <narmingale@gmail.com>
2021-10-17 19:15:00 +0400

    Structured Sidebar component

--- 12-Sidebar-Modal/src/Sidebar.js
+++ 12-Sidebar-Modal/src/Sidebar.js
@@ -1,7 +1,42 @@
 import React from 'react'
+import logo from './me-logo.png'
+import { FaTimes } from 'react-icons/fa'
+import { social, links } from './data'
 
 function Sidebar() {
-  return <div></div>
+  return (
+    <aside className={`sidebar show-sidebar`}>
+      <div className="sidebar-header">
+        <img src={logo} className="logo" alt="my logo" />
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+      <ul className="social-icons">
+        {social.map((link) => {
+          const { id, url, icon } = link
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          )
+        })}
+      </ul>
+    </aside>
+  )
 }
 
 export default Sidebar
