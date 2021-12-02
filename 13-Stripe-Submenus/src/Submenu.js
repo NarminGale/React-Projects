2403111bc04fe26dc33db57661f1f9f8e79b6bf7
NarminGale <narmingale@gmail.com>
2021-12-02 19:21:49 +0400

    Setup submenu

--- 13-Stripe-Submenus/src/Submenu.js
+++ 13-Stripe-Submenus/src/Submenu.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import { useGlobalContext } from './context'
 
 function Submenu() {
-  return <div></div>
+  const { isSubmenuOpen } = useGlobalContext()
+
+  return (
+    <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}>
+      submenu
+    </aside>
+  )
 }
 
 export default Submenu
