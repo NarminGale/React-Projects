23170490f69695a1265cfba52c1f09af9edd8c69
NarminGale <narmingale@gmail.com>
2021-12-02 19:22:40 +0400

    Gave functionality to menu buttons

--- 13-Stripe-Submenus/src/Navbar.js
+++ 13-Stripe-Submenus/src/Navbar.js
@@ -6,6 +6,11 @@ import { useGlobalContext } from './context'
 function Navbar() {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext()
 
+  const displaySubmenu = (e) => {
+    console.log('helloo')
+    openSubmenu()
+  }
+
   return (
     <nav>
       <div className="nav-center">
@@ -17,13 +22,19 @@ function Navbar() {
         </div>
         <ul className="nav-links">
           <li>
-            <button className="link-btn">products</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              products
+            </button>
           </li>
           <li>
-            <button className="link-btn">developers</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              developers
+            </button>
           </li>
           <li>
-            <button className="link-btn">company</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              company
+            </button>
           </li>
         </ul>
         <button className="btn signin-btn">Sign in</button>
